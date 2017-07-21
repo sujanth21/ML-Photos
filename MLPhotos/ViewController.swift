af16945b71e976d28ed2373007c1e46a419db14e
Sujanth <sujanth1987@gmail.com>
2017-07-21 13:30:26 +1000

    Added results method to handle model request

--- MLPhotos/ViewController.swift
+++ MLPhotos/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "cat2.jpg", ofType: "jpg")
+        let imageURL = NSURL.fileURL(withPath: path!)
+        
+        let modelFile = GoogLeNetPlaces()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        let handler = VNImageRequestHandler(url: imageURL)
+        let request = VNCoreMLRequest(model: model, completionHandler: myResultsMethod)
+        
+        try! handler.perform([ request ])
+        
+    }
+    
+    func myResultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation]
+            else {
+               fatalError("Could not find results in ML Vision request!")
+            }
+        
+        var bestPrediction = ""
+        var bestConfidence: VNConfidence = 0
+        
+        for classification in results {
+            if classification.confidence > bestConfidence {
+                bestConfidence = classification.confidence
+                bestPrediction = classification.identifier
+            }
+        }
+                
     }
 
     override func didReceiveMemoryWarning() {
